173a4c24a33e4ae5489ac68a712ccd9cebeea9d2
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-10-02 17:53:48 +0200

    It works

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 Prend des input
 
 ex:
-debut : 7h30
-fin   : 10h30
-duree : 3h
+debut : 8h32
+fin   : 12h45
+08h32 - 12h45  4:13:00   4:13:00
 
-debut : 11h30
-fin   : 12h00
-duree : 3h30
+debut : 13h45
+fin   : 18
+08h32 - 12h45  4:13:00   4:13:00
+13h45 - 18h00  4:15:00   8:28:00
 """
-from datetime import datetime
+from datetime import datetime, timedelta
+import sys
 
 
 def to_datetime(_str):
@@ -23,20 +25,40 @@ def to_datetime(_str):
             return datetime.strptime(_str, "%H")
 
 
-def run():
-    print("exemple :  8h40")
-    print("           9:00")
-    print("          16h40")
-    print()
-
+def add_date(date_list):
     debut = input("debut : ")
     d_debut = to_datetime(debut)
 
     fin = input("fin   : ")
     d_fin = to_datetime(fin)
+    date_list.append([d_debut, d_fin])
 
-    print("duree :", d_fin - d_debut)
+
+def hour_sec(_datetime):
+    return datetime.strftime(_datetime, "%Hh%M")
+
+
+def show_diff(date_list):
+    total = timedelta()
+    for tt1, tt2 in date_list:
+        local = tt2 - tt1
+        total = total + local
+        print(f"{hour_sec(tt1)} - {hour_sec(tt2)}  {local}   {total}")
+
+
+def run():
+    print("exemple : 8h40 9:00 16h40 16")
+    date_list = []
+
+    while True:
+        print()
+        add_date(date_list)
+        print()
+        show_diff(date_list)
 
 
 if __name__ == "__main__":
-    run()
+    try:
+        run()
+    except KeyboardInterrupt:
+        sys.exit(0)
