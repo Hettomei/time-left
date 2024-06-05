cfcdabbade471517de6946f750f6cea33ce40eb3
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2024-06-05 17:17:16 +0200

    Write in file at the end

--- main.py
+++ main.py
@@ -11,8 +11,32 @@ fin   : 18
 08h32 - 12h45  4:13:00   4:13:00
 13h45 - 18h00  4:15:00   8:28:00
 """
+
+import argparse
 from datetime import datetime, timedelta
 import sys
+
+# import os
+# SEPARATOR = os.linesep
+SEPARATOR = "\n"
+# SEPARATOR = "\r\n"
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description="Affiche un chrono",
+    )
+
+    parser.add_argument(
+        "--append-to",
+        "-a",
+        dest="append_to",
+        default="",
+        help="Au moment de quitter, ecris le resultat à la fin du fichier",
+        metavar="/d/code/file.txt",
+    )
+
+    return parser.parse_args(args)
 
 
 def to_datetime(_str, pattern):
@@ -39,7 +63,6 @@ def to_datetime_full(_str):
     raise ValueError(f"No date for {str_time}")
 
 
-
 def add_date(date_list):
     debut = input("debut : ")
     d_debut = to_datetime_full(debut)
@@ -53,27 +76,60 @@ def hour_sec(_datetime):
     return datetime.strftime(_datetime, "%Hh%M")
 
 
-def show_diff(date_list):
+def diff_to_list(date_list):
     total = timedelta()
+    lines = []
     for tt1, tt2 in date_list:
         local = tt2 - tt1
         total = total + local
-        print(f"{hour_sec(tt1)} - {hour_sec(tt2)}  {local}   {total}")
+        lines.append(f"{hour_sec(tt1)} - {hour_sec(tt2)}  {local}   {total}")
+
+    return lines
 
 
-def run():
-    print("exemple : 8h40 9:00 16h40 16 ou 9 15 pour 9h15")
-    date_list = []
+def diff_to_string(date_list):
+    return SEPARATOR.join(diff_to_list(date_list))
 
+
+def ask_time(date_list):
     while True:
         print()
         add_date(date_list)
         print()
-        show_diff(date_list)
+        print(diff_to_string(date_list))
+
+
+def append_to(params, date_list):
+    if not params.append_to:
+        return None
+
+    with open(params.append_to, "a", encoding="utf-8", newline=SEPARATOR) as myfile:
+        myfile.writelines(
+            [
+                SEPARATOR,
+                datetime.strftime(datetime.now(), "# %Y-%m-%d %A"),
+                SEPARATOR,
+                SEPARATOR,
+                diff_to_string(date_list),
+                SEPARATOR,
+                SEPARATOR,
+            ]
+        )
+
+    return True
+
+
+def run(args):
+    params = parse_args(args)
+    print("exemple : 8h40 - 9:00 - 16h40 - 16 - 9 15 - pour 9h15")
+    date_list = []
+
+    try:
+        ask_time(date_list)
+    except KeyboardInterrupt:
+        append_to(params, date_list)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    try:
-        run()
-    except KeyboardInterrupt:
-        sys.exit(0)
+    run(sys.argv[1::1])
