bf5e8ab2ffc739a6aaa18a8070180ab05d91499a
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2026-01-15 23:48:31 +0100

    add test

--- main_test.py
+++ main_test.py
@@ -11,3 +11,24 @@ def test_to_datetime_full():
     assert main.hour_sec(main.to_datetime_full("11 32")) == "11h32"
     assert main.hour_sec(main.to_datetime_full("    12    34  ")) == "12h34"
     assert main.hour_sec(main.to_datetime_full("    13:35  ")) == "13h35"
+
+
+def test_full_integration():
+    date_list = []
+    t = main.to_datetime_full
+
+    date_list.append([t("0h"), t("0")])
+    date_list.append([t("13:35"), t("13:35")])
+    date_list.append([t("1"), t("1 3")])
+    date_list.append([t("8"), t("18 7")])
+    date_list.append([t("7 30"), t("18h20")])
+    date_list.append([t("1"), t("4")])
+    date_list.append([t("1"), t("4")])
+
+    assert main.diff_to_string(date_list) == """00h00 - 00h00  0:00:00   0:00:00
+13h35 - 13h35  0:00:00   0:00:00
+01h00 - 01h03  0:03:00   0:03:00
+08h00 - 18h07  10:07:00   10:10:00
+07h30 - 18h20  10:50:00   21:00:00
+01h00 - 04h00  3:00:00   1 day, 0:00:00
+01h00 - 04h00  3:00:00   1 day, 3:00:00"""
