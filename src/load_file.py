d5b6893154a3903dcbacdd03d7d213a337c5749f
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2026-02-07 01:34:45 +0100

    better parse, better test

--- src/load_file.py
+++ src/load_file.py
@@ -4,16 +4,18 @@ import re
 
 SEPARATOR: str = "\n"
 
-extract_delta = re.compile(r"^(\d\d:\d\d:\d\d) - (\d\d:\d\d:\d\d)")
+extract_delta = re.compile(r"^(\S+) - (\S+)")
 
 
 def load_file(user_data: UserData):
     """
-    Efface le contenu de user_data.date_list
-    tente de charger avec les valeur.
-    ne leve pas d erreur
+    tente de charger les valeurs.
+    Si une valeur est trouvée :
+    - efface le contenu de user_data.date_list
+    - remplit avec les nouvelles data
+
+    Cette fonction ne leve pas d erreur
     """
-    user_data.clear_list()
     initial_date_match = user_data.format_current_date()
 
     state = "nothing"
@@ -26,13 +28,16 @@ def load_file(user_data: UserData):
 
             # ensuite on cherche un timedelta
             if state == "found_date" and extract_delta.match(s_line.rstrip()):
+                user_data.clear_list()
                 state = "found_datedelta"
 
             # Ici, toutes les lignes consecutive doivent matcher
             # sinon, on s arrete
             if state == "found_datedelta":
+                # print("line: " + s_line.rstrip())
                 deltas = extract_delta.match(s_line.rstrip())
                 if deltas:
+                    # print("g1:" + deltas.group(1) + " -- g2:" + deltas.group(2))
                     user_data.append(
                         text_to_datetime(deltas.group(1)),
                         text_to_datetime(deltas.group(2)),
--- src/load_file_test.py
+++ src/load_file_test.py
@@ -30,17 +30,9 @@ def test_load_file_when_file_load_data():
     u.append(text_to_datetime("10m"), text_to_datetime("20m"))
     u.write_in_file()
 
-    assert len(u.date_list) == 2
+    u.clear_list()
     load_file(u)
     assert len(u.date_list) == 2
 
     assert u.date_list[0] == [text_to_datetime("8h"), text_to_datetime("9h")]
     assert u.date_list[1] == [text_to_datetime("10m"), text_to_datetime("20m")]
-
-
-def test_load_file_cas_complexe():
-    a = tempfile.NamedTemporaryFile(delete=False)
-    u = UserData(a.name)
-    u.append(text_to_datetime("8h"), text_to_datetime("7"))
-
-    # Alors devra afficher 23h
